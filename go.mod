e85a1bbe0e70b3d27b29ce6a55b0c739d2fe8f6b
Fabian <Fabian009.berndt@gmail.com>
2024-11-13 10:42:19 +0100

    starting cache

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ephim135/pokedexApi.git
+
+go 1.23.3
---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,49 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	mu sync.Mutex
+	cacheEntries map[string]cacheEntry
+	interval time.Duration
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val []byte
+}
+
+func NewCache(interval time.Duration) *Cache {
+	cache := &Cache{
+		mu: var mu sync.Mutex
+		items: make(map[string]cacheEntry),
+		interval: interval,
+	}
+	go cache.startCleanup()
+	return cache
+}
+
+func (c Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.cacheEntries[key].val = val
+	c.cacheEntries[key].createdAt = time.Now()
+}
+
+func (c Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.cacheEntries[key]
+}
+
+func (c Cache) startCleanup() (){
+	ticker := time.NewTicker(c.interval)
+	for range ticker.C {
+		c.mu.Lock()
+		defer c.mu.Unlock()
+
+	}
+}
